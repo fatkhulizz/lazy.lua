7dc3f5a9e5b12503a2722a70c1a5b4af1c78da1f
fatkhulizz <fatkhul7izza@gmail.com>
2023-10-04 22:22:38 +0700

    remove unneeded plugin

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  -- { "folke/noice.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.bufremove", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+}
