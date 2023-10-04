f2beea9adb7d04fab3c420f0707185b9bcc2f234
fatkhulizz <fatkhul7izza@gmail.com>
2023-10-04 22:58:45 +0700

    change general settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,41 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- [[Useful/hack Keymaps]]
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- set ; as :
+vim.keymap.set("n", ";", ":")
+
+-- keep cursor in center
+vim.keymap.set("n", "J", "mzJ'z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep original yanked when pasted
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- yanked and delete text to clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>d", '"+d')
+vim.keymap.set("v", "<leader>d", '"+d')
+
+-- navigate within insert mode
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+
+-- assign esc to clear highlight
+vim.keymap.set("n", "<Esc>", ":noh <CR>")
+
+-- remove Q
+vim.keymap.set("n", "Q", "<nop>")
