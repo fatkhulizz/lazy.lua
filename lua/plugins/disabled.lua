34f8aa2fa36c9e0ff55e49f8ac97954faf14a58c
fatkhulizz <fatkhul7izza@gmail.com>
2023-10-07 23:41:11 +0700

    disable more plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,5 +5,9 @@ return {
   { "echasnovski/mini.bufremove", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
 }
