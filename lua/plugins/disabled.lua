7ba29136dcda704f3e3143b7f614a2ef1a327dd6
fatkhul7izza@gmail.com <fuzz@wimbows.168.1.3>
2023-11-23 17:35:25 +0700

    disable dashboard

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -9,5 +9,6 @@ return {
   { "RRethy/vim-illuminate", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
 }
