8709ca092873e6364ccdb370c3862410977768d0
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-12 22:15:21 +0700

    add cmaketools

--- lua/plugins/cmaketools.lua
+++ lua/plugins/cmaketools.lua
@@ -1,8 +1,18 @@
 return {
   {
     "Civitasv/cmake-tools.nvim",
+    opts = {},
+    event = "LazyFile",
     keys = {
       { "<leader>cb", "<cmd>CMakeQuickBuild<cr>", desc = "CMake Quick Build" },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "cmake" })
+      end
+    end,
   },
 }
