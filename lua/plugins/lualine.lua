23cd29df543bb86be99e4de185d2348457ca63e3
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-12 09:07:44 +0700

    add lsp things on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,37 @@ return {
     local icons = require("lazyvim.config").icons
     local Util = require("lazyvim.util")
 
+    local function lsp_progress()
+      local messages = vim.lsp.util.get_progress_messages()
+      if #messages == 0 then
+        return ""
+      end
+      local status = {}
+      for _, msg in pairs(messages) do
+        table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
+      end
+      local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+      local ms = vim.loop.hrtime() / 1000000
+      local frame = math.floor(ms / 120) % #spinners
+      return table.concat(status, " | ") .. " " .. spinners[frame + 1]
+    end
+
+    local function lsp_name()
+      local msg = ""
+      local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+      local clients = vim.lsp.get_active_clients()
+      if next(clients) == nil then
+        return msg
+      end
+      for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+          return client.name
+        end
+      end
+      return msg
+    end
+
     return {
       options = {
         theme = "auto",
@@ -49,6 +80,9 @@ return {
             cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
             color = Util.ui.fg("Debug"),
           },
+          { lsp_progress },
+          { lsp_name, icon = "  LSP:" },
+          -- stylua: ignore
           { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.ui.fg("Special") },
           {
             "diff",
