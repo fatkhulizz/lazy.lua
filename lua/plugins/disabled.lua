879d5231e3304c091f37a53ac8f968d0609ad5bc
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-13 09:03:55 +0700

    disable more plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,14 +1,22 @@
 return {
-  -- { "folke/noice.nvim", enabled = false },
+
+  --[[ wont use ]]
+  { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+
+  --[[ replaced by another plugin ]]
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "RRethy/vim-illuminate", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
+
+  --[[ maybe useful ( someday ) ]]
+  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
 }
